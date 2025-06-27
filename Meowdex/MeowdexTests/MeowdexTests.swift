d3678822cb76c6e225bfe58621d4492abec31e07
João Valente <joao.valente@ceiia.com>
2025-06-27 18:52:35 +0100

    creating project from template

---
+++ Meowdex/MeowdexTests/MeowdexTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeowdexTests.swift
+//  MeowdexTests
+//
+//  Created by Johnnie on 27/06/2025.
+//
+
+import Testing
+@testable import Meowdex
+
+struct MeowdexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
